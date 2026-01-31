8a4751766c24e89a0344b886fb57f2b00150c700
Mons00n <cael.corredera.perez@gmail.com>
2026-02-01 00:06:04 +0100

    Public record

---
+++ Stock.java
@@ -0,0 +1,4 @@
+public record Stock (int num, String cuenta){ //Solo para el transporte de datos o respuestas de APIs ya que son inmutables. No puede heredar de otras clases. 
+    //Es un constructor compacto que se ejecuta antes de asignar los valores. Dejar de escribir código repetitivo para clases que solo almacenan datos.
+    
+}
